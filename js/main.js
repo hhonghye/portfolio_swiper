e266eb8d728e7eb17429462864839a24ee9eaa2e
hhonghye <honghye194@gmail.com>
2021-07-27 22:49:43 +0900

    changeBg() - prev, next, mousewheel, navi, slide 적용

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 const menu = ["first", "second", "third", "fourth"];
-
+//swiper 연결
 const swiper = new Swiper('#swiper-container', {
     loop: true, 
     centeredSlides: true,
     slidesPerView: 'auto', //동시에 보여줄 슬라이드 갯수 
     mousewheel: true,
+    // grabCursor: true,
 
     navigation: {
         nextEl: ".swiper-button-next",
@@ -20,3 +21,35 @@ const swiper = new Swiper('#swiper-container', {
       }
     },
   });
+
+/*----- DOM Caching -----*/
+const bgs = document.querySelectorAll(".background li");
+const prev = document.querySelector(".swiper-button-prev");
+const next = document.querySelector(".swiper-button-next");
+const navi = document.querySelectorAll(".swiper-pagination span");
+
+/*----- function -----*/
+
+/*----- event -----*/
+prev.addEventListener("click", changeBg);
+next.addEventListener("click", changeBg);
+window.addEventListener("mousewheel", changeBg);
+swiper.on("slideChangeTransitionStart", changeBg);
+for(el of navi){
+    el.addEventListener("click", e=>{
+        let isOn = e.currentTarget.classList.contains("swiper-pagination-bullet-active");
+        if(isOn) return;
+        swiper.on("slideChangeTransitionStart", changeBg);
+    })
+    
+}
+
+/*----- function definition -----*/
+function changeBg(){
+    let item = document.querySelector(".swiper-slide-active");
+    let i = item.getAttribute("data-swiper-slide-index");
+    for(el of bgs){
+        el.classList.remove("on");
+    }
+    bgs[i].classList.add("on");
+}
